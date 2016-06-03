9e52f8672bbc42e626532fd336bb633547c04eb2
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-06-03 19:20:16 +0200

    updated sass

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,19 @@
 import * as soundworks from 'soundworks/server';
 import BeatsExperience from './BeatsExperience';
 
-soundworks.server.init({ appName: 'Beats' });
+const config = {
+  appName: 'Beats',
+  // name of the environement, (use NODE_ENV=production to configure express at the same time.)
+  env: (process.env.NODE_ENV || 'development'),
+};
+
+soundworks.server.init(config);
 
 // define the configuration object to be passed to the `.ejs` template
 soundworks.server.setClientConfigDefinition((clientType, config, httpRequest) => {
   return {
     clientType: clientType,
+    env: config.env,
     socketIO: config.socketIO,
     appName: config.appName,
     version: config.version,
